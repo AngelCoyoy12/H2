f72b484269a04962e59b319e8e72cf09f488fc8f
Angel Coyoy <furioso7s12@gmail.com>
2024-08-08 11:20:58 -0600

    First commmit resources

---
+++ src/main/java/Memoria_con_H2/MemoriaH2/MemoriaH2Application.java
@@ -0,0 +1,13 @@
+package Memoria_con_H2.MemoriaH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriaH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Memoria_con_H2/MemoriaH2/MemoriaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Memoria_con_H2.MemoriaH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
